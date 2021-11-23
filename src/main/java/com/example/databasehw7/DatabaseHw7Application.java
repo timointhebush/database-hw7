c26668d4db6198b3386813e522c5c3b00e1a2e75
timointhebush <junzzang97@naver.com>
2021-11-23 14:47:57 +0900

    first commit

---
+++ src/main/java/com/example/databasehw7/DatabaseHw7Application.java
@@ -0,0 +1,13 @@
+package com.example.databasehw7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseHw7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseHw7Application.class, args);
+	}
+
+}
